9f61db8a8d4a7d3ea4e316fcbb07f64666a2c1c2
Celio Junior <celio_cjr@hotmail.com>
2024-10-07 09:08:55 +0930

    feat: creating formulary and save/close buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,64 @@
 import logo from './assets/logo-in-orbit.svg'
 import letsStart from './assets/lets-start-illustration.svg'
-import { Plus } from 'lucide-react'
+import { Plus, X } from 'lucide-react'
+import { Button } from './components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from './components/ui/dialog'
+import { DialogClose } from '@radix-ui/react-dialog'
 
 export function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="in orbit logo" />
-      <img
-        src={letsStart}
-        alt="home page illustration of woman launching a rocket"
-      />
-      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        You dont have any goal registered yet, but you can start right now!
-        Please, click in the botton below and start creating your goals.
-      </p>
+    <Dialog>
+      <div className="h-screen flex flex-col items-center justify-center gap-8">
+        <img src={logo} alt="in orbit logo" />
+        <img
+          src={letsStart}
+          alt="home page illustration of woman launching a rocket"
+        />
+        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+          You haven't set any goals yet, how about setting one right now?
+        </p>
+        <DialogTrigger asChild>
+          <Button>
+            <Plus className="s-4" />
+            Set goal
+          </Button>
+        </DialogTrigger>
+      </div>
 
-      <button
-        type="button"
-        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
-      >
-        <Plus className="s-4" />
-        Create goal
-      </button>
-    </div>
+      <DialogContent>
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-3">
+            <div className="flex items-center justify-between">
+              <DialogTitle>Create goal</DialogTitle>
+              <DialogClose>
+                <X className="size-5 text-zinc-600" />
+              </DialogClose>
+            </div>
+
+            <DialogDescription>
+              Add activities that make you feel good and that you want to
+              continue practicing every week.
+            </DialogDescription>
+          </div>
+          <form className="flex-1 flex flex-col justify-between">
+            <div>oi</div>
+            <div className="flex items-center gap-3">
+              <DialogClose asChild>
+                <Button type="button" className="flex-1" variant="secondary">
+                  Close
+                </Button>
+              </DialogClose>
+              <Button className="flex-1">Save</Button>
+            </div>
+          </form>
+        </div>
+      </DialogContent>
+    </Dialog>
   )
 }
