6aa598dd4191b9efc36e16e0b224b71c7086648c
Celio Junior <celio_cjr@hotmail.com>
2024-10-07 09:38:06 +0930

    feat: finishing form and setting style for RadioGroupInd. component.

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ import {
   DialogTrigger,
 } from './components/ui/dialog'
 import { DialogClose } from '@radix-ui/react-dialog'
+import { Label } from './components/ui/label'
+import { Input } from './components/ui/input'
+import { RadioGroup, RadioGroupIndicator, RadioGroupItem } from './radio-group'
 
 export function App() {
   return (
@@ -32,7 +35,7 @@ export function App() {
       </div>
 
       <DialogContent>
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-6 h-full">
           <div className="flex flex-col gap-3">
             <div className="flex items-center justify-between">
               <DialogTitle>Create goal</DialogTitle>
@@ -47,7 +50,70 @@ export function App() {
             </DialogDescription>
           </div>
           <form className="flex-1 flex flex-col justify-between">
-            <div>oi</div>
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col gap-2">
+                <Label htmlFor="title">What's the activity?</Label>
+                <Input
+                  id="title"
+                  autoFocus
+                  placeholder="Working out, meditate, study..."
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <Label htmlFor="title">How many time in the week?</Label>
+                <RadioGroup>
+                  <RadioGroupItem value="1">
+                    <RadioGroupIndicator />
+                    <span className="text-zinc-300 text-sm font-medium leading-none">
+                      1x week
+                    </span>
+                    <span className="text-lg leading-none">🥱</span>
+                  </RadioGroupItem>
+                  <RadioGroupItem value="1">
+                    <RadioGroupIndicator />
+                    <span className="text-zinc-300 text-sm font-medium leading-none">
+                      2x week
+                    </span>
+                    <span className="text-lg leading-none">🙂</span>
+                  </RadioGroupItem>
+                  <RadioGroupItem value="1">
+                    <RadioGroupIndicator />
+                    <span className="text-zinc-300 text-sm font-medium leading-none">
+                      3x week
+                    </span>
+                    <span className="text-lg leading-none">😎</span>
+                  </RadioGroupItem>
+                  <RadioGroupItem value="1">
+                    <RadioGroupIndicator />
+                    <span className="text-zinc-300 text-sm font-medium leading-none">
+                      4x week
+                    </span>
+                    <span className="text-lg leading-none">😜</span>
+                  </RadioGroupItem>
+                  <RadioGroupItem value="1">
+                    <RadioGroupIndicator />
+                    <span className="text-zinc-300 text-sm font-medium leading-none">
+                      5x week
+                    </span>
+                    <span className="text-lg leading-none">🤨</span>
+                  </RadioGroupItem>
+                  <RadioGroupItem value="1">
+                    <RadioGroupIndicator />
+                    <span className="text-zinc-300 text-sm font-medium leading-none">
+                      6x week
+                    </span>
+                    <span className="text-lg leading-none">🤯</span>
+                  </RadioGroupItem>
+                  <RadioGroupItem value="1">
+                    <RadioGroupIndicator />
+                    <span className="text-zinc-300 text-sm font-medium leading-none">
+                      Every day
+                    </span>
+                    <span className="text-lg leading-none">🔥</span>
+                  </RadioGroupItem>
+                </RadioGroup>
+              </div>
+            </div>
             <div className="flex items-center gap-3">
               <DialogClose asChild>
                 <Button type="button" className="flex-1" variant="secondary">
